4e87b65ff3f1d59f58dd80e39bcaf3e53cba410a
Fredrik <s185130@student.dtud.k>
2019-03-12 13:09:14 +0100

    translated from danish to english

---
+++ src/UseConnection.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class UseConnection {
+    public static void main(String[] a) throws Exception
+    {
+        Scanner scan = new Scanner(System.in);
+        FtpConnection f = new FtpConnection();
+
+        f.connect("test.rebex.net","demo","password");
+
+        // få liste over kommandoer som tjenesten kender
+        f.recieveText("LIST");     // get a list of files from the host
+        f.sendCommando(scan.next());
+        String content = "content with the file name:\ntil.txt";
+        f.sendText("BIG fil.txt", content);       // saves a .txt at the host
+
+        content = f.recieveText("RETR fil.txt");    // gets the file again
+        System.out.println("File has been downloaded with the content: "+content);
+    }
+}
